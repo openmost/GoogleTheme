bb58f1d74d6b5f0c936824661308f6b9f8a6221d
Ronan <ronan56.hello@gmail.com>
2022-05-22 20:23:06 +0200

    update: fonts, widgets and cards UI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v1.2
+- Change Font
+- Updating lateral menu UI
+- Updating cards UI
+- Updating widgets UI
+
 ## v1.1.0
 - Support Matomo 4.x
 - Fix CSS padding for cards and tables
--- GoogleTheme.php
+++ GoogleTheme.php
@@ -13,7 +13,8 @@ use Piwik\Plugin;
 
 class GoogleTheme extends Plugin
 {
-    public function getListHooksRegistered()
+
+    public function registerEvents()
     {
         return [
             'Theme.configureThemeVariables' => 'configureThemeVariables',
@@ -22,6 +23,34 @@ class GoogleTheme extends Plugin
 
     public function configureThemeVariables(Plugin\ThemeStyles $vars)
     {
-        //Nothing here
+        $vars->fontFamilyBase = '"Google Sans", Arial, Helvetica, sans-serif';
+        $vars->colorBrand = '#1a73e8';
+        $vars->colorBrandContrast = '#fff';
+        $vars->colorText = '#212121';
+        $vars->colorTextLight = '#444';
+        $vars->colorTextLighter = '#666666';
+        $vars->colorTextContrast = '#37474f';
+        $vars->colorLink = '#1976D2';
+        $vars->colorBaseSeries = '#ee3024';
+        $vars->colorHeadlineAlternative = '#4E4E4E';
+        $vars->colorHeaderBackground = '#ffffff';
+        $vars->colorHeaderText = '#5f6368';
+        $vars->colorMenuContrastText = '#202124';
+        $vars->colorMenuContrastTextSelected = '#1a73e8';
+        $vars->colorMenuContrastTextActive = '#3450A3';
+        //$vars->colorMenuContrastBackground = '';
+        $vars->colorWidgetExportedBackgroundBase = '#eff0f1';
+        $vars->colorWidgetTitleText = '#333';
+        //vars->colorWidgetTitleBackground = '';
+        $vars->colorBackgroundBase = '#f8f9fa';
+        $vars->colorBackgroundTinyContrast = '#f2f2f2';
+        $vars->colorBackgroundLowContrast = '#d9d9d9';
+        $vars->colorBackgroundContrast = '#fff';
+        $vars->colorBackgroundHighContrast = '#202020';
+        $vars->colorBorder = '#cccccc';
+        $vars->colorCode = '#f3f3f3';
+        $vars->colorCodeBackground = '#4d4d4d';
+        //$vars->colorWidgetBackground = '#ffffff';
+        //$vars->colorWidgetBorder = '';
     }
 }
